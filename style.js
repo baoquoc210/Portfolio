c3d6605d08613d90dc4745dba8b1fae34bab76d3
Huynh Quoc Bao <12quocbao@gmai.com>
2024-12-27 13:39:52 +0700

    change snow effect

--- style.js
+++ style.js
@@ -106,7 +106,6 @@ document.addEventListener('DOMContentLoaded', function() {
 //Snow Flake
 const NUMBER_OF_SNOWFLAKES = 300;
 const MAX_SNOWFLAKE_SPEED = 2;
-const MAX_SNOWFLAKE_SIZE = 5;
 const SNOWFLAKE_COLOUR = '#ddd';
 const snowflakes = [];
 
@@ -123,27 +122,25 @@ const ctx = canvas.getContext('2d');
 const createSnowflake = () => ({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
-    radius: Math.floor(Math.random() * MAX_SNOWFLAKE_SIZE) +1,      
-    colour: SNOWFLAKE_COLOUR,
-    speed: Math.random() * MAX_SNOWFLAKE_SPEED + 3,
+    size: Math.random() * 20 + 10, // Snowflake size range
+    speed: Math.random() * MAX_SNOWFLAKE_SPEED + 1,
     sway: Math.random() - 0.5
 });
 
 const drawSnowflake = snowflake => {
-    ctx.beginPath();
-    ctx.arc(snowflake.x, snowflake.y, snowflake.radius, 0, Math.PI * 2);
-    ctx.fillStyle = snowflake.colour;
-    ctx.fill();
-    ctx.closePath();
-}
+    ctx.fillStyle = SNOWFLAKE_COLOUR;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText('❄', snowflake.x, snowflake.y);
+};
 
 const updateSnowflake = snowflake => {
     snowflake.y += snowflake.speed;
     snowflake.x += snowflake.sway;
     if (snowflake.y > canvas.height) {
-        Object.assign(snowflake, createSnowflake());
+        Object.assign(snowflake, createSnowflake(), { y: -snowflake.size });
     }
-}
+};
 
 const animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -152,9 +149,9 @@ const animate = () => {
         updateSnowflake(snowflake);
         drawSnowflake(snowflake);
     });
-    
+
     requestAnimationFrame(animate);
-}
+};
 
 for (let i = 0; i < NUMBER_OF_SNOWFLAKES; i++) {
     snowflakes.push(createSnowflake());
